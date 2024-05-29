04cfb968f665e321ff9eadaa8b8fa079267114ae
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-28 18:29:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    entity: "aidenaiden8",
+  users: { // You can add multiple users by doing username: 'password'. 
+    entity: 'aidenaiden8',
+    orbet: 'ae45',
   },
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
 }
 export default config
