4741047ffb97da51ba5dce6934e43158d602b3cf
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-05-30 08:16:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const config = {
   users: { // You can add multiple users by doing username: 'password'. 
     entity: 'aidenaiden8',
     orbet: 'ae45',
-    K1654: 'Ef5'
+    K1654: 'Ef5',
+    N246: 'Nana1'
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
