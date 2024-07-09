8e0f8372280a287229df18c87c35d50b799341e1
Entity <85138593+acesavagejr@users.noreply.github.com>
2024-07-09 13:11:52 -0500

    new (school) year new passwords!

--- config.js
+++ config.js
@@ -2,9 +2,6 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: { // You can add multiple users by doing username: 'password'. 
     entity: 'aidenaiden8',
-    orbet: 'ae45',
-    K1654: 'Ef5',
-    N246: 'Nana1'
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
